a5a997b61ac22480f5ee2cbfdec36d4e4c42fa78
cdarguello <159964129+cdarguello@users.noreply.github.com>
2024-06-17 16:20:11 -0600

    Añadido modo de curvas múltiples y generación de outputs en una carpeta

--- XFLR5 foil to SolidWorks curve converter.py
+++ XFLR5 foil to SolidWorks curve converter.py
@@ -4,11 +4,13 @@
 
 import pandas as pd
 import math
+import os
+import shutil
 
 # Variables globales
 arc_output = "D:\Documentos\AeronauTEC\Convertidor perfil XFLR5 a SolidWorks\output_perfil.txt"
 
-def aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_diedro, offsetX, offsetY, offsetZ):
+def aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_diedro, offsetX, offsetY, offsetZ, i, anguloDiedro):
     global arc_output
     columnas = ['x','y','z']
     # Se reordenan las coordenadas tal que la curva aparezca en el plano solicitado
@@ -37,25 +39,59 @@ def aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_
     df[df.columns[1]] = og * math.sin(anguloZ) + df[df.columns[1]] *  math.cos(anguloZ)
 
     # Se calcula el offset automáticamente en caso de que fuera solicitado, respecto al ángulo del diedro
-    if diedro:
+    if diedro and i == None:
         if eje == 'Z':
             if abs(offsetX) >= abs(inicio_diedro):
                 offsetY = abs(offsetX - inicio_diedro) / math.sin(math.pi/2-anguloZ) * math.sin(anguloZ)
         elif eje == 'X':
             if abs(offsetZ) >= abs(inicio_diedro):
                 offsetY = abs(offsetZ - inicio_diedro) / math.sin(math.pi/2-anguloX) * math.sin(anguloX)
-            
-        
+    elif diedro and i != None:
+        if eje == 'Z':
+            if abs(offsetX) >= abs(inicio_diedro):
+                offsetY = abs(offsetX - inicio_diedro) / math.sin(math.pi/2-anguloDiedro) * math.sin(anguloDiedro)
+                og = df[df.columns[0]]
+                df[df.columns[0]] = df[df.columns[0]] * math.cos(anguloDiedro) - df[df.columns[1]] * math.sin(anguloDiedro)
+                df[df.columns[1]] = og * math.sin(anguloDiedro) + df[df.columns[1]] *  math.cos(anguloDiedro)
+        elif eje == 'X':
+            if abs(offsetZ) >= abs(inicio_diedro):
+                offsetY = abs(offsetZ - inicio_diedro) / math.sin(math.pi/2-anguloDiedro) * math.sin(anguloDiedro)       
+                og = df[df.columns[1]]
+                df[df.columns[1]] = df[df.columns[1]] * math.cos(anguloDiedro) - df[df.columns[2]] * math.sin(anguloDiedro)
+                df[df.columns[2]] = og * math.sin(anguloDiedro) + df[df.columns[2]] *  math.cos(anguloDiedro)
     # Se añade el offset solicitado a cada coordenada
     df[df.columns[0]] += offsetX
     df[df.columns[1]] += offsetY
     df[df.columns[2]] += offsetZ
+
+    # Obtener los path relevantes
+    n = len(arc_output) -1
+    for c in reversed(arc_output):
+        if c == '\\':
+            break
+        n -= 1
+    path = arc_output[:n+1]
+    file_name = arc_output[n+1:]
+    dir_path = os.path.join(path, "output")
+
+    # Creación en limpio de la carpeta de output
+    if i == 0 or i == None:
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+            os.mkdir(dir_path)
     #Se actualiza el archivo de salida
-    df.to_csv(arc_output,index=False, header=False)
+    if i != None:
+        if not isinstance(i, str):
+            df.to_csv(dir_path+"\\"+file_name[:-4]+"_"+str(i+1)+".txt",index=False, header=False)
+        else:
+            df.to_csv(dir_path+"\\"+file_name[:-4]+"_"+i+".txt",index=False, header=False)
+    else:
+        df.to_csv(dir_path+"\\"+file_name,index=False, header=False)
 
 def main():
     global arc_output
     # Inicialización de varibles con valores default
+    modo = 0 #Modo 0 es modo simple, y modo 1 es el modo de curvas múltiples
     archivo = "D:\Documentos\AeronauTEC\Convertidor perfil XFLR5 a SolidWorks\perfil.dat"
     cuerda = 250
     eje = 'Z'
@@ -70,7 +106,15 @@ def main():
     linea_mod = []
     diedro = False
     inicio_diedro = 0
+    finAla = 200
+    numCurvas = 5
+    anguloDiedro = 0
+
     # Se reciben los datos de entrada y son validados
+    modoinput = input("Ingrese 1 si quiere usar el modo de curvas múltiples o 0 si quiere el modo simple (default): ")
+    if modoinput != '':
+        modo = int(modoinput)
+
     arcinput = input("Nombre del archivo por convertir junto con su extension, debe estar en la misma carpeta que este script (default: perfil.dat): ")
     if arcinput != '':
         archivo = arcinput
@@ -123,6 +167,17 @@ def main():
     if offsetZinput != '':
         offsetZ = float(offsetZinput)
 
+    if modo == 1:
+        finAlainput = input("Coordenada transversal donde termina el ala: ")
+        if finAlainput != '':
+            finAla = float(finAlainput)
+        numCurvasinput = input("Numero de curvas por generar: ")
+        if numCurvasinput != '':
+            numCurvas = int(numCurvasinput)
+        anguloDiedroinput = input("Ángulo del diedro: ")
+        if anguloDiedroinput != '':
+            anguloDiedro = math.radians(float(anguloDiedroinput))
+
     # Preprocesamiento de los datos (transformación al formato requerido por SolidWorks)    
     try:
         with open(archivo, "r+") as arc:
@@ -155,7 +210,42 @@ def main():
             mod.write(linea)
 
     # Se le aplican a los datos las modificaciones adicionales solicitadas
-    aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_diedro, offsetX, offsetY, offsetZ)
+    if modo == 0: #En el modo simple se llama normal a la función
+        aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_diedro, offsetX, offsetY, offsetZ, None, anguloDiedro)
+    else: # En el modo de curvas múltiples se llama iterativamente a la función de modificaciones para crear el número de curvas solicitado
+        if eje == 'Z':
+            # Se calcula la distancia transversal a la que debe estar cada curva
+            dist = (finAla - offsetX) / (numCurvas-1)
+            offsetX_inicio = offsetX
+            # Se van creando perfiles con un offset transversal cada vez mayor, hasta llegar a un offset igual a donde termina el ala
+            for i in range(numCurvas):
+                if i == numCurvas-1:
+                    offsetX = finAla
+                else:
+                    offsetX = offsetX_inicio + dist*i
+                aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_diedro, offsetX, offsetY, offsetZ, i, anguloDiedro)
+            if diedro:
+                offsetX = inicio_diedro
+                aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_diedro, offsetX, offsetY, offsetZ, "diedro", anguloDiedro)
+        else:
+            dist = (finAla - offsetZ) / (numCurvas-1)
+            offsetZ_inicio = offsetZ
+            for i in range(numCurvas):
+                if i == numCurvas-1:
+                    offsetZ = finAla
+                else:
+                    offsetZ = offsetZ_inicio + dist*i
+                aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_diedro, offsetX, offsetY, offsetZ, i, anguloDiedro)
+            if diedro:
+                offsetZ = inicio_diedro
+                aplicarModificaciones(eje, plano, anguloX, anguloY, anguloZ, diedro, inicio_diedro, offsetX, offsetY, offsetZ, "diedro", anguloDiedro)
+    
+    #Eliminación del archivo temporal
+    try:
+        os.remove(arc_output)
+    except OSError as error:
+        print(error)
+        print("No se pudo eliminar el archivo temporal")
 
     print("El archivo creado se llama ", arc_output,".")
     input("Presione Enter para terminar el programa")
